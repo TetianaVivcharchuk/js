69c6c1aee889fe3e681abb5f052a46d94cdcdfd9
TetianaVivcharchuk <129064247+TetianaVivcharchuk@users.noreply.github.com>
2023-03-27 16:29:51 +0300

    Add files via upload

---
+++ 1js/math.js
@@ -0,0 +1,24 @@
+
+//1 exercise
+ let firstNumber = Number(prompt('First value'));
+ let secondNumber = Number(prompt('Second value'));
+ let thirdNumber = Number(prompt('Third value'));
+ let plus = Number(firstNumber)  + Number(secondNumber)+Number(thirdNumber);
+ let result =Number(plus)/3;
+ console.log('Середнє арифметичне ');
+ console.log(result);
+
+
+//2exercise
+let oneNumber = Number(prompt('First value'));
+ let twoNumber = Number(prompt('Second value'));
+ let compare =Number(oneNumber) == Number(twoNumber);
+
+ console.log(compare);
+
+// 3exercise
+
+let saleNumber = Number(prompt('Введіть значення знижки (цифрове)'));
+
+let endNumber=12999-(12999*saleNumber);
+console.log(endNumber);
